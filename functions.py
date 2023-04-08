67d05a0681d97e7414879f5a87457ea10de341f5
mohammad fathi <m.fathi3742@gmail.com>
2023-04-08 21:20:41 +0330

    add functions

---
+++ functions.py
@@ -0,0 +1,30 @@
+from netmiko import ConnectHandler
+
+# Define the device details
+device = {
+    'device_type': 'cisco_ios',
+    'ip': 'ip',  # Replace with the switch IP address
+    'username': 'admin',
+    'password': 'Skills39~!@',
+}
+
+# Define the interface details
+device["ip"]=input("Enter Device IP: ")
+interface = input("Enter Interface ID: ")
+
+# Connect to the device
+with ConnectHandler(**device) as net_connect:
+    # Enter privileged EXEC mode
+    net_connect.enable()
+    
+    # Configure the interface in access mode
+    config_commands = [
+        f'interface GigabitEthernet{interface}',
+        'no  authentication port-control auto',
+        'no dot1x pae authenticator',
+        'do wr',
+    ]
+    output = net_connect.send_config_set(config_commands)
+    
+    # Print the output
+    print(output)

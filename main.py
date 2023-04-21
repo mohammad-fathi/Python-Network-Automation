2822dd0018c34985a9aa3056068e4eddcfbecc20
mohammad fathi <m.fathi3742@gmail.com>
2023-04-21 16:18:27 +0330

    add trunk module and change some vlan func

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ white=Fore.WHITE
 green=Fore.GREEN
 reset=Fore.RESET
 
-from modules import Hostname,vlan
+from modules import Hostname,vlan,trunk
 import functions , os , getpass
 
 # Call Functions Main Menu
@@ -64,7 +64,41 @@ while True:
                     }
             
                     # Call Module VALN 
-                    vlan.vlan_class.hostname_func(device_info=device_info,new_vlan=new_vlan)
+                    vlan.vlan_class.vlan_func(device_info=device_info,new_vlan=new_vlan)
+
+        elif num=="3":
+            os.system("clear" or "cls")
+            
+            # Read all ip switch from iplist.txt 
+            with open('iplist.txt', 'r') as f:
+                ip_list = f.readlines()
+
+                # Connect to Switches in List
+                for ip in ip_list:
+
+                    # Strip any whitespace characters from the IP
+                    ip = ip.strip()
+
+                    # Get Information Device from User
+                    user=input(f"Enter Username Device {ip}: ")
+                    password=getpass.getpass(f"Enter Password Device {ip}: ")
+                    print(f"""
+                    {red} Help: Please Enter full name interface
+                    {white}Example:
+                    {green}FastEthernet0/1 {reset}or {green} GigabitEthernet0/1 {reset}""")
+                    
+                    trunkport=input("Enter Trunk Ports: ")
+                    device_info={
+                        'device_type': 'cisco_ios',
+                        'ip': ip ,
+                        'username': user,
+                        'password': password,
+                    }
+                    # Call Module Trunk 
+                    trunk.trunk_class.trunk_func(device_info=device_info,trunk_port=trunkport)   
+
+    
+                    
 
 
     except Exception as e:
--- modules/trunk.py
+++ modules/trunk.py
@@ -1,14 +1,15 @@
 from netmiko import ConnectHandler
 
-class vlan_class:
-    def vlan_func(device_info,new_vlan):
+class trunk_class:
+    def trunk_func(device_info,trunk_port):
         try:
             # Connect to Device
             with ConnectHandler(**device_info) as net_connect:
                 net_connect.enable()
 
-                # New VLAN
-                config_commands = [f'vlan {new_vlan}',
+                # Config Trunk Port
+                config_commands = [f'interface {trunk_port}',
+                                   "switchport mode trunk",
                                    "do wr"]
                 output = net_connect.send_config_set(config_commands)
                 net_connect.disconnect()
